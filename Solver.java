aa4779b1f81658939363ec4c5c2fa160b0dd23d0
ClaudiaLainfiesta <24000149@galileo.edu>
2025-02-20 19:21:26 -0600

    Comparador de caminos

--- Solver.java
+++ Solver.java
@@ -3,83 +3,35 @@
     public String[] solve(Maze maze)
     pero es libre de crear otros metodos y clases en este u otro archivo que desee.
 */
-public class Solver {
-    private Maze maze;
+public class Solver{
 
-    public Solver(Maze maze) {
+    private String[] caminos;
+
+    public Solver(String[] caminos){
         //Sientase libre de implementar el contructor de la forma que usted lo desee
-        this.maze = maze;
+    }
+
+    private void busquedaSalidas(){
+
+    }
+
+    public String caminoMasCorto(){
+        if(caminos == null || caminos.length == 0){
+            return "[]";
+        }
+
+        String masCorto = caminos[0];
+        for(String opcion : caminos){
+            if(opcion.length() < masCorto.length()){
+                masCorto = opcion;
+            }
+        }
+        return masCorto;
     }
 
     public String solve(Maze maze){
         //Implemente su metodo aqui. Sientase libre de implementar métodos adicionales
-        return "[-1]";
+        return caminoMasCorto();
     }
 
-    //Metodo que nos dice que camara tiene peligro y cual no.
-    public void peligro() {
-        Node[][][] nodes = maze.getNodes();
-        Node[][][] safeNodes = new Node[maze.getWidth()][maze.getHeight()][maze.getDepth()];
-
-        for (int z = 0; z < maze.getDepth(); z++) {
-            for (int y = 0; y < maze.getHeight(); y++) {
-                for (int x = 0; x < maze.getWidth(); x++) {
-                    Node currentNode = nodes[x][y][z];
-
-                    if (!currentNode.danger) {
-                        safeNodes[x][y][z] = currentNode;
-                    }
-                }
-            }
-        }
-        Maze safeMaze = new Maze(safeNodes, maze.getWidth(), maze.getHeight(), maze.getDepth());
-
-        // for (int z = 0; z < maze.getDepth(); z++) {
-        //     for (int y = 0; y < maze.getHeight(); y++) {
-        //         for (int x = 0; x < maze.getWidth(); x++) {
-        //             Node currentNode = safeNodes[x][y][z];
-
-        //             if (currentNode != null) {
-        //                 System.out.println("Cámara segura en (" + x + ", " + y + ", " + z + ")");
-        //             }
-        //         }
-        //     }
-        // }
-        System.out.println("Maze original:");
-        for (int z = 0; z < maze.getDepth(); z++) {
-            for (int y = 0; y < maze.getHeight(); y++) {
-                for (int x = 0; x < maze.getWidth(); x++) {
-                    Node currentNode = nodes[x][y][z];
-
-                    if (currentNode.danger) {
-                        System.out.println("Cámara en (" + x + ", " + y + ", " + z + ") tiene peligro.");
-                    } else {
-                        System.out.println("Cámara en (" + x + ", " + y + ", " + z + ") está segura.");
-                    }
-                }
-            }
-        }
-        System.out.println("Maze ya seguro:");
-        for (int z2 = 0; z2 < safeMaze.getDepth(); z2++) {
-            for (int y2 = 0; y2 < safeMaze.getHeight(); y2++) {
-                for (int x2 = 0; x2 < safeMaze.getWidth(); x2++) {
-                    Node currentNode2 = nodes[x2][y2][z2];
-
-                    if (currentNode2.danger) {
-                        System.out.println("Cámara en (" + x2 + ", " + y2 + ", " + z2 + ") tiene peligro.");
-                    } else {
-                        System.out.println("Cámara en (" + x2 + ", " + y2 + ", " + z2 + ") está segura.");
-                    }
-                }
-            }
-        }
-    }
-
-        public static void main(String[] args) throws Exception {
-            Maze maze = new Maze("prueba.txt");
-            Solver solver = new Solver(maze);
-            solver.peligro();
-        }
-
-    
 }
---
+++ pruebaComparador.java
@@ -0,0 +1,7 @@
+public class pruebaComparador {
+    public static void main(String[] args) {
+        String[] words = {"EUS", "TTTT", "S", "SYTSDF", "ST"};
+        Solver prueba = new Solver(words);
+        System.out.println(prueba.caminoMasCorto());
+    }
+}
